2d55db643998d2153d518450ff2dd2c9b46b9fa7
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2022-09-16 09:00:05 +0200

    style: Updates style configs and CI jobs (#9)
    * style: Updated style configs
    
    * docs: Added makefile
    
    * ci: Updated CI jobs
    
    * ci: Fixed CI
    
    * style: Fixed style of convert script

--- convert.py
+++ convert.py
@@ -21,7 +21,7 @@ def main(args):
     if not dest_folder.is_dir():
         raise FileNotFoundError(f"unable to access: {dest_folder}")
     # Read the file list
-    with open(dest_folder.joinpath('rst-files.txt'), 'r') as f:
+    with open(dest_folder.joinpath("rst-files.txt"), "r") as f:
         rst_files = [line.strip() for line in f.readlines()]
 
     # Download all the files
@@ -33,18 +33,19 @@ def main(args):
         if (response.status_code // 100) != 2:
             raise ValueError(f"Unable to retrieve {file_url}")
         tmp_file = f"tmp_{file}"
-        with open(tmp_file, 'wb') as f:
+        with open(tmp_file, "wb") as f:
             f.write(response.content)
-        dest_file = os.path.join(args.dest, str(file).replace('.rst', '.ipynb'))
+        dest_file = os.path.join(args.dest, str(file).replace(".rst", ".ipynb"))
         # Convert it
         os.system(f"rst2ipynb {tmp_file} -o {dest_file}")
         # Delete tmp file
         os.remove(tmp_file)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='RST to notebook conversion',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="RST to notebook conversion", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument("origin", type=str, help="URL to the folder with RST files")
     parser.add_argument("dest", type=str, help="Where the converted files should be places")
     args = parser.parse_args()
